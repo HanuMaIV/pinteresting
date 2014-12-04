ba0a8fa78d5100abd9ac2adbbd8a2f957a07cba3
Brendan Miranda <me@brendanmiranda.com>
2014-12-03 22:18:56 -0800

    installed and configured devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  #required for heroku, note to set this to our actual hostname
+  config.action_mailer.default_url_options = { host: 'indianinthecupboard.com' }
 end
